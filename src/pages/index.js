e9b793210e4725fcfb50b0434dae6fcc9b4bdbf2
matthewapower <matthewallenpower@gmail.comm>
2020-06-24 15:44:39 -0400

    Make the homepage responsive

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React, { useRef } from "react"
 import { graphql } from "gatsby"
+import { css } from "@emotion/core"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -13,11 +14,12 @@ import "react-glidejs/dist/index.css"
 const IndexPage = ({ data }) => {
   const content = data.allContentfulHomePage.nodes[0]
   const gliderRef = useRef(null)
+  const desktop = typeof window !== "undefined" && window.innerWidth > 768
   let parallax
   return (
     <Layout>
       <SEO title="Home" />
-      <Parallax pages={2.4} ref={ref => (parallax = ref)} className="bg-white">
+      <Parallax pages={desktop ? 3 : 3.2} ref={ref => (parallax = ref)}>
         <ParallaxLayer
           offset={0}
           speed={0.5}
@@ -25,29 +27,23 @@ const IndexPage = ({ data }) => {
           style={{
             background: `url(${content.mainBackground.fluid.src}) center center/cover`,
           }}
-        >
-          <section
-            className="pt-16 h-full"
-            style={{
-              background: `url(${content.mainBackground.fluid.src}) center center/cover`,
-            }}
-          />
-        </ParallaxLayer>
+          className="pt-16 h-full"
+        ></ParallaxLayer>
         <ParallaxLayer offset={0.1} speed={1.5}>
           <img
             src={logo}
             alt="Buckhead Butcher Shop"
-            className="mx-auto max-w-sm"
+            className="mx-auto w-64 md:w-full md:max-w-sm"
           />
         </ParallaxLayer>
 
         <ParallaxLayer offset={0.9} speed={1} factor={0.5}>
           <section>
-            <div className="bg-secondary text-white max-w-4xl p-12">
-              <h1 className="font-display text-4xl tracking-wider">
+            <div className="bg-secondary text-white text-center md: text-left max-w-4xl py-12 px-4 md:p-12">
+              <h1 className="font-display text-2xl md:text-4xl tracking-wider leading-normal">
                 {content.heroTitle}
               </h1>
-              <p className="font-sans text-lg">
+              <p className="font-sans text-base md:text-lg">
                 {content.heroParagraph.internal.content}
               </p>
             </div>
@@ -55,69 +51,79 @@ const IndexPage = ({ data }) => {
         </ParallaxLayer>
         <ParallaxLayer
           offset={1}
-          speed={1}
-          factor={0.5}
-          className="flex items-center justify-center"
+          speed={desktop ? 0.5 : 0.8}
+          factor={desktop ? 1 : 1.5}
+          className="flex flex-col items-center justify-center bg-white pt-20 max-w-full"
         >
-          <section id="menu" className="bg-white">
-            <Glide
-              ref={gliderRef}
-              type="slider"
-              perView={5}
-              startAt={0}
-              focusAt="0"
-              bound={true}
-              className="container mx-auto"
-            >
-              {content.sliderImages.map((s, i) => (
-                <div
-                  key={i}
-                  style={{
-                    background: `url(${s.fluid.src}) center center/cover`,
-                    height: "400px",
-                  }}
-                />
-              ))}
-            </Glide>
-
-            <div className="container mx-auto my-16 flex flex-col items-center justify-center text-black">
-              <h2 className="text-center font-display text-4xl tracking-wider">
-                {content.sliderTitle}
-              </h2>
-              <p className="max-w-3xl mx-auto font-sans">
-                {content.sliderDescription.sliderDescription}
-              </p>
-              <a
-                href={content.menu.file.url}
-                className="font-display uppercase bg-secondary text-white mx-auto inline-block py-2 px-12"
-              >
-                View Our Menu
-              </a>
-            </div>
-          </section>
-        </ParallaxLayer>
-        <ParallaxLayer offset={1.9} speed={1.2}>
-          <section
-            className="py-20 absolute inset-0"
-            style={{
-              background: `url(${content.contactBackground.fluid.src}) center center/cover`,
+          <Glide
+            ref={gliderRef}
+            type="slider"
+            perView={5}
+            startAt={0}
+            focusAt="0"
+            bound={true}
+            breakpoints={{
+              768: {
+                perView: 1,
+              },
             }}
+            className="container mx-auto"
+            css={css`
+              .glide__slides {
+                margin-left: 0;
+              }
+              min-height: 400px;
+            `}
           >
-            <div className="container mx-auto my-16 flex flex-col items-center justify-center max-w-3xl text-white">
-              <h2 className="text-center font-display text-4xl tracking-wider">
-                {content.contactTitle}
-              </h2>
-              <p className="mx-auto font-sans">
-                {content.contactDescription.contactDescription}
-              </p>
-              <Form />
-            </div>
-            <img
-              className="absolute bottom-0 right-0 mb-0"
-              src={content.contactAccentImage.fluid.src}
-              alt="Buckhead Butcher Shop"
-            />
-          </section>
+            {content.sliderImages.map((s, i) => (
+              <div
+                key={i}
+                style={{
+                  background: `url(${s.fluid.src}) center center/cover`,
+                  height: "400px",
+                }}
+              />
+            ))}
+          </Glide>
+
+          <div className="container px-4 md:mx-auto my-16 flex flex-col">
+            <h2 className="text-center font-display text-2xl md:text-4xl leading-relaxed tracking-wider">
+              {content.sliderTitle}
+            </h2>
+            <p className="w-full max-w-3xl mx-auto font-sans">
+              {content.sliderDescription.sliderDescription}
+            </p>
+            <a
+              href={content.menu.file.url}
+              className="font-display uppercase bg-secondary text-white mx-auto py-2 px-12"
+            >
+              View Our Menu
+            </a>
+          </div>
+        </ParallaxLayer>
+        <ParallaxLayer
+          offset={2}
+          speed={desktop ? 0.3 : 0.5}
+          factor={desktop ? 1 : 1.3}
+          className="py-20 flex items-center justify-center"
+          style={{
+            background: `url(${content.contactBackground.fluid.src}) center center/cover`,
+          }}
+        >
+          <div className="container p-4 mx-auto my-16 flex flex-col items-center justify-center max-w-3xl text-white">
+            <h2 className="text-center font-display text-2xl md:text-4xl tracking-wider">
+              {content.contactTitle}
+            </h2>
+            <p className="mx-auto font-sans text-sm">
+              {content.contactDescription.contactDescription}
+            </p>
+            <Form />
+          </div>
+          <img
+            className="hidden md:block absolute bottom-0 right-0 mb-0"
+            src={content.contactAccentImage.fluid.src}
+            alt="Buckhead Butcher Shop"
+          />
         </ParallaxLayer>
       </Parallax>
     </Layout>
