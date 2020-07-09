da707a0ab954985d2a5e1a5afad48614494f565c
matthewapower <matthewallenpower@gmail.comm>
2020-07-09 16:05:33 -0400

    Add success message to the form.

--- src/components/form.js
+++ src/components/form.js
@@ -10,6 +10,7 @@ export default function Form(props) {
   }
 
   const [submission, setSubmission] = useState({})
+  const [submitted, setSubmitted] = useState(false)
 
   const handleChange = e => {
     setSubmission({ ...submission, [e.target.name]: e.target.value })
@@ -17,7 +18,6 @@ export default function Form(props) {
 
   const handleSubmit = e => {
     const form = e.target
-    console.log("starting the submit")
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
@@ -26,57 +26,60 @@ export default function Form(props) {
         ...submission,
       }),
     })
-      .then(() => console.log("halfway through"))
+      .then(() => setSubmitted(true))
       .catch(error => alert(error))
     e.preventDefault()
-    console.log("ending the submit")
   }
 
-  return (
-    <form
-      name="subscribeForm"
-      method="post"
-      action="/success"
-      data-netlify="true"
-      data-netlify-honeypot="bot-field"
-      onSubmit={handleSubmit}
-      id="subscribe-form"
-      className="flex flex-col w-full"
-    >
-      <input type="hidden" name="form-name" value="subscribeForm" />
-      <p hidden>
-        <label>
-          Don’t fill this out: <input name="bot-field" />
+  if (submitted === false) {
+    return (
+      <form
+        name="subscribeForm"
+        method="post"
+        action="/success"
+        data-netlify="true"
+        data-netlify-honeypot="bot-field"
+        onSubmit={handleSubmit}
+        id="subscribe-form"
+        className="flex flex-col w-full"
+      >
+        <input type="hidden" name="form-name" value="subscribeForm" />
+        <p hidden>
+          <label>
+            Don’t fill this out: <input name="bot-field" />
+          </label>
+        </p>
+        <div className="grid grid-cols-2 gap-4 w-full mb-12">
+          <TextInput change={handleChange} name={"name"} type="text" />
+          <TextInput change={handleChange} name={"email"} type="email" />
+        </div>
+        <label htmlFor="newsletter" className="mb-2">
+          <input
+            type="checkbox"
+            name="newsletter"
+            id="newsletter"
+            onChange={handleChange}
+            className="mr-4"
+          />
+          Please sign me up for Buckhead Butcher Shop’s monthly newsletter.
         </label>
-      </p>
-      <div className="grid grid-cols-2 gap-4 w-full mb-12">
-        <TextInput change={handleChange} name={"name"} type="text" />
-        <TextInput change={handleChange} name={"email"} type="email" />
-      </div>
-      <label htmlFor="newsletter" className="mb-2">
-        <input
-          type="checkbox"
-          name="newsletter"
-          id="newsletter"
-          onChange={handleChange}
-          className="mr-4"
-        />
-        Please sign me up for Buckhead Butcher Shop’s monthly newsletter.
-      </label>
-      <label htmlFor="promotions" className="mb-8">
-        <input
-          type="checkbox"
-          name="promotions"
-          id="promotions"
-          className="mr-4"
-          onChange={handleChange}
-        />
-        Please sign me up for Buckhead Butcher Shop’s exclusive sales and
-        promotional notices.
-      </label>
-      <button className="font-display uppercase bg-secondary text-white mx-auto inline-block py-2 px-12">
-        Submit
-      </button>
-    </form>
-  )
+        <label htmlFor="promotions" className="mb-8">
+          <input
+            type="checkbox"
+            name="promotions"
+            id="promotions"
+            className="mr-4"
+            onChange={handleChange}
+          />
+          Please sign me up for Buckhead Butcher Shop’s exclusive sales and
+          promotional notices.
+        </label>
+        <button className="font-display uppercase bg-secondary text-white mx-auto inline-block py-2 px-12">
+          Submit
+        </button>
+      </form>
+    )
+  } else {
+    return <h2 className="font-display uppercase">Thanks!</h2>
+  }
 }
